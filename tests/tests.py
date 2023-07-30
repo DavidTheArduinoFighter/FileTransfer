c7afd96ded4a877d928bb0c80f38e8babe63842a
DavidTheArduinoFighter <65223452+DavidTheArduinoFighter@users.noreply.github.com>
2023-07-30 22:19:05 +0200

    test updated

--- tests/tests.py
+++ tests/tests.py
@@ -2,9 +2,9 @@ import src.encryption as enc
 import os
 
 
-def test_encryption():
+def test_encryption_folder():
     dir_list = os.listdir('test_data/video')
-    original = []
+    original = ['']
     for file in dir_list:
         with open(f'test_data/video/{file}', 'rb') as test_file:
             original.append(test_file.read())
@@ -23,5 +23,16 @@ def test_encryption():
     assert [True, True, True] == test_out
 
 
+def test_encryption_file():
+    with open('test_data/bled-zima-jezero-grad-cerkev.jpg', 'rb') as test_file:
+        original = test_file.read()
+
+    test_enc = enc.Encryption()
+    key = test_enc.new_key()
+    enc_file = test_enc.encrypt(original)
+    dec_file = test_enc.decrypt(key, enc_file)
+
+    assert dec_file == enc_file
+
 if __name__ == '__main__':
-    test_encryption()
+    test_encryption_folder()
