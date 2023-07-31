f2bfd2a55c887dd4c52d2fe504c29a5a90147430
DavidTheArduinoFighter <65223452+DavidTheArduinoFighter@users.noreply.github.com>
2023-07-31 21:53:25 +0200

    test for encryption and decryption works

--- tests/tests.py
+++ tests/tests.py
@@ -1,26 +1,30 @@
+import sys
+
+# adding Folder_2 to the system path
+sys.path.insert(0, 'C:/Users/david/Documents/GitHub/FileTransfer/FileTransfer')
 import src.encryption as enc
 import os
 
 
 def test_encryption_folder():
+    test_enc = enc.Encryption()
+    key = test_enc.new_key()
     dir_list = os.listdir('test_data/video')
-    original = ['']
+    original = []
     for file in dir_list:
         with open(f'test_data/video/{file}', 'rb') as test_file:
             original.append(test_file.read())
 
-    test_enc = enc.Encryption()
-    key = test_enc.new_key()
     test_out = []
     for file in original:
         enc_file = test_enc.encrypt(file)
+        with open('video_enc.mkv', 'wb') as encrypted_file:
+            encrypted_file.write(enc_file)
         dec_file = test_enc.decrypt(key, enc_file)
-        if dec_file == original:
-            test_out.append(True)
-        else:
-            test_out.append(False)
+        with open('video_dec.mkv', 'wb') as decrypt_file:
+            decrypt_file.write(dec_file)
 
-    assert [True, True, True] == test_out
+    assert original[0] == dec_file
 
 
 def test_encryption_file():
